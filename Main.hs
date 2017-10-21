d3f0e96fe2f486b1cf3c79dcb3dfb169cb2872b8
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-10-21 14:54:15 -0400

    se arregla la firma y return statement de crearOraculo

--- Main.hs
+++ Main.hs
@@ -1,13 +1,13 @@
 import qualified Oraculo as O
 import qualified Data.Map as M
 
-crearOraculo :: IO ()
+crearOraculo :: IO O.Oraculo
 crearOraculo = do
 	putStrLn "Para crear un oráculo desde cero, ingrese una predicción:"
 	prediccion <- getLine
 	putStrLn  "Se ha creado un oraculo con la siguiente predicción:"
 	putStrLn prediccion
-	return IO $ O.crearOraculo prediccion
+	return $ O.crearOraculo prediccion
 
 preguntaFinal :: String -> String
 preguntaFinal s = "Estás pensando en..." ++ s ++ "? [s/n]"
@@ -30,43 +30,42 @@ predecir :: O.Oraculo -> IO ()
 predecir (O.Prediccion s) = do
 	putStrLn $ preguntaFinal s
 	respuesta <- getLine
-	-- when (head respuesta != 's') {- Agregar Oraculo -} 
+	-- when (head respuesta != 's') {- Agregar Oraculo -}
 	salir
 predecir (O.Pregunta p ops) = do
 	putStrLn p
 	mapM_ (\(s,o) -> putStrLn '-':s) ops
 	respuesta <- getLine
-	-- when (respuesta == "Ninguna") IO () -- Npi de qué debe hacer	
-	if M.member respuesta ops 
-		then 
+	-- when (respuesta == "Ninguna") IO () -- Npi de qué debe hacer
+	if M.member respuesta ops
+		then
 			let o = lookup respuesta ops
 				in predecir o
-		else 
+		else
 			predecir (O.Pregunta p ops)
-	
-	
+
+
 
 salir :: IO()
 salir = putStrLn "Gracias por usar el glorioso Haskinator!"
 
 opciones = M.fromList [ ("Crear Oráculo Nuevo",        crearOraculo),
-							("Hacer una Predicción",       predecir),
-							("Guardar el Oráculo actual",  persistir),
-							("Cargar un Oráculo",          cargar),
-							("Consultar Pregunta Crucial", crucial),
-							("Salir",                      salir)] 
+												("Hacer una Predicción",       predecir),
+												("Guardar el Oráculo actual",  persistir),
+												("Cargar un Oráculo",          cargar),
+												("Consultar Pregunta Crucial", crucial),
+												("Salir",                      salir)]
 
 fakeMain = do
 	putStrLn "Por favor, elige una opción:"
 	mapM_ (\(s,_) -> putStrLn '-':s) opciones
 	respuesta <- getLine
-	if M.member respuesta opciones 
+	if M.member respuesta opciones
 		then return $ M.lookup respuesta opciones
-		else do 
+		else do
 			putStrLn "...Hmmm... No entendí lo que quieres."
-			putStrLn "Escribe la opción que deseas" 
+			putStrLn "Escribe la opción que deseas"
 			fakeMain
 
 main = do
 	putStrLn "Bienvenido al glorioso Haskinator!"
-
